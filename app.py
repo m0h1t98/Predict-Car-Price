5a8f8ba2921beffbbce60039afb11d4d7237a4d8
Mohit Agrawal <52734794+m0h1t98@users.noreply.github.com>
2020-07-17 20:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/',methods=['GET'])
+@app.route('/carpricepredictive.herokuapp.com',methods=['GET'])
 
 def Home():
     return render_template('index.html')
